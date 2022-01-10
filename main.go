c3453d8a866b84413fceaf5ba686338ff1e4dbbd
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-01-10 18:19:59 -0500

    side-effect in main

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
-	identity "github.com/ic-matcom/model-identity-go/api"
+	_ "github.com/ic-matcom/model-identity-go/api"
 	_ "github.com/ic-matcom/model-identity-go/tools"
 )
 
 func main() {
-	fmt.Println("probando...", identity.ContractNameIdentity)
+
 }
 
