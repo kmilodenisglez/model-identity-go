2704440c9edf3dbb0fe0598e01c1b79f2408b6e4
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-01-13 19:41:43 -0500

    add RichQuerySelector for Participant and Issuer

--- model/common.go
+++ model/common.go
@@ -21,3 +21,21 @@ type TransactionD struct {
 	Document  string `json:"document"`
 }
 
+type ParticipantRichQuerySelector struct {
+	Selector struct {
+		DocType   string `json:"docType"`
+		ID        string `json:"id,omitempty" metadata:",optional"`  // participant id: used in the composite key to store the participant in the ledger
+		Did       string `json:"did,omitempty" metadata:",optional"` // "did:hash_public_key"
+		PublicKey string `json:"publicKey,omitempty" metadata:",optional"`
+		Active    bool   `json:"active,omitempty" metadata:",optional"`
+	} `json:"selector"`
+}
+
+type IssuerRichQuerySelector struct {
+	Selector struct {
+		DocType string `json:"docType"`
+		ID      string `json:"id,omitempty" metadata:",optional"` // participant id: used in the composite key to store the participant in the ledger
+		Name    string `json:"name,omitempty" metadata:",optional"`
+		Active  bool   `json:"active,omitempty" metadata:",optional"`
+	} `json:"selector"`
+}
