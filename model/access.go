95bff8864ec054b75810938793be1d54bea3fd52
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-01-13 20:13:59 -0500

    added AccessCreateRequest and AccessUpdateRequest

--- model/access.go
+++ model/access.go
@@ -6,3 +6,15 @@ type AccessResponse struct {
 	Description       string   `json:"description,omitempty" metadata:",optional"` // access description
 	ContractFunctions []string `json:"contractFunctions,omitempty"`                // contract functions
 }
+
+// AccessCreateRequest
+type AccessCreateRequest struct {
+	ContractName      string   `json:"contractName"`
+	ContractFunctions []string `json:"contractFunctions,omitempty"` // contract functions
+}
+
+// AccessUpdateRequest
+type AccessUpdateRequest struct {
+	ID                string   `json:"id"`
+	ContractFunctions []string `json:"contractFunctions"` // contract functions name
+}
